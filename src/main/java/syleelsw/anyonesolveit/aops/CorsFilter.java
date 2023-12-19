4bcb8c1a287244c361348b4cf377a04006b0906c
이석우 <iseog-u@iseog-uui-MacBookAir.local>
2023-12-19 15:13:56 +0900

    feat: add CORS FIlter

---
+++ src/main/java/syleelsw/anyonesolveit/aops/CorsFilter.java
@@ -0,0 +1,36 @@
+package syleelsw.anyonesolveit.aops;
+
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.boot.web.servlet.filter.OrderedFilter;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+@Order(OrderedFilter.HIGHEST_PRECEDENCE)
+public class CorsFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletResponse response = (HttpServletResponse) res;
+        String origin = request.getHeader("Origin");
+        response.setHeader("Access-Control-Allow-Origin", "https://fe.anyone-pe.kr");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Allow-Methods","*");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers",
+                "Origin, X-Requested-With, Content-Type, Accept, Authorization, Access, RefreshToken, refresh, Refresh");
+        response.setHeader("Cross-Origin-Opener-Policy", "same-origin");
+        response.setHeader("Access-Control-Expose-Headers","Access, RefreshToken" );
+        if("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+            response.setStatus(HttpServletResponse.SC_OK);
+        }else {
+            chain.doFilter(req, res);
+        }
+
+    }
+}
