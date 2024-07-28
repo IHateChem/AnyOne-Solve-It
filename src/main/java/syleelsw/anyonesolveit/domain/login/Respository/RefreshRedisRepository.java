0d08c503568de1ad5f6644abeba4e4150caf20d4
이석우 <iseog-u@iseog-uui-MacBookAir.local>
2024-07-28 20:35:44 +0900

    [fix] 검색 fhrm

--- src/main/java/syleelsw/anyonesolveit/domain/login/Respository/RefreshRedisRepository.java
+++ src/main/java/syleelsw/anyonesolveit/domain/login/Respository/RefreshRedisRepository.java
@@ -1,8 +1,8 @@
 package syleelsw.anyonesolveit.domain.login.Respository;
 
 
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.repository.CrudRepository;
 import syleelsw.anyonesolveit.domain.login.RefreshEntity;
-
 public interface RefreshRedisRepository extends CrudRepository<RefreshEntity, Long> {
 }
