2853c185a2bfca4cd096f64633db5f904db3876d
이석우 <iseog-u@iseog-uui-MacBookAir.local>
2024-07-28 20:02:15 +0900

    [fix] 검색 인코딩 수정

--- src/main/java/syleelsw/anyonesolveit/service/study/StudyService.java
+++ src/main/java/syleelsw/anyonesolveit/service/study/StudyService.java
@@ -613,7 +613,8 @@ public class StudyService {
         String urlString = "https://solved.ac/api/v3/search/problem?query=" + prefix+ query;
 
         try {
-            String url = URLEncoder.encode(urlString, "UTF-8");
+            String url = urlString.replaceAll(" ", "%20").replaceAll("#", "%23").replaceAll("\\+", "%2B")
+                    .replaceAll("@", "%40");
             log.info("prefix: {}, query: {}, sum: {}, url: {}",prefix, query,  prefix +"+"+ query, url);
             RestTemplate restTemplate = new RestTemplate();
             ResponseEntity<SolvedProblemPages> response = restTemplate.getForEntity(url, SolvedProblemPages.class);
